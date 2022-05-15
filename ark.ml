2b2f40097e621fbcb987f0caec22ad4a7cef40a3
Ronith Kandallu <ronithkandallu@gmail.com>
2022-05-15 11:35:58 -0700

    integrate read_input function into compiler pipeline

--- ark.ml
+++ ark.ml
@@ -1,22 +1,25 @@
-let safe_read_line() =
-  try Some(read_line()) with
-  End_of_file -> None
-
-let read_input =
-  let rec read_input_helper input =
-    match safe_read_line() with
-    | Some(line) -> read_input_helper (input ^ "\n" ^ line)
-    | None -> input
-  in
-  read_input_helper ""
-
 let () =
   let speclist = [] in
   let usage_msg = "usage: ./ark.native [-a|-s|-l] [file.mc]" in
   let channel = ref stdin in
   Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
 
-  let lexbuf = Lexing.from_channel !channel in
+  let read_input chan =
+    let safe_read_line chan =
+      try Some(input_line chan) with
+      End_of_file -> None
+    in
+
+    let rec read_input_helper input chan =
+      match safe_read_line chan with
+      | Some(line) -> read_input_helper (input ^ "\n" ^ line) chan
+      | None -> input
+    in
+    read_input_helper "" chan
+  in
+
+  let source = read_input !channel in
+  let lexbuf = Lexing.from_string source in
   let ast = Parser.program Scanner.tokenize lexbuf in
   let sast = Semantics.check ast in
   let codegen = Irgen.translate sast in
